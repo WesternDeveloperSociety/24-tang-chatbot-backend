9dd7e86528346dfbdd0434a2b6f39ec547c17fc1
benjaminnamayandev <benjaminnamayandeh@gmail.com>
2025-03-19 14:54:45 -0400

    Added component for mappedin

--- FloorplanNavigation/src/App.tsx
+++ FloorplanNavigation/src/App.tsx
@@ -2,14 +2,7 @@ import React from 'react';
 import { MapView, useMapData, useMap, Label } from '@mappedin/react-sdk';
 import '@mappedin/react-sdk/lib/esm/index.css';
 import { Space } from '@mappedin/react-sdk/mappedin-js/src';
-
-function MyCustomComponent() {
-  const { mapData } = useMap();
-
-  return mapData.getByType('space').map((space) => (
-    <Label key={space.id} target={space.center} text={space.name} />
-  ));
-}
+import MappedIn from './components/mappedin'
 
 export default function App() {
 
@@ -30,7 +23,7 @@ export default function App() {
 
   return mapData ? (
     <MapView mapData={mapData}>
-      <MyCustomComponent />
+      <MappedIn />
     </MapView>
   ) : null;
 }
